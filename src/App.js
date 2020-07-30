f9cad636522179a97ef094f88ddb279a4693f5b5
rnovick13 <rnovick13@gmail.com>
2020-07-30 13:33:57 -0400

    props section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+// import React from 'react';
+// import { Text, View } from 'react-native';
+//
+// const HelloWorldApp = () => {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center"
+//       }}>
+//       <Text>Hello, world!</Text>
+//     </View>
+//   )
+// }
 
-const HelloWorldApp = () => {
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center'
+  }
+})
+
+const Greeting = (props) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
+    <View style={styles.center}>
+      <Text>Hello {props.name}!</Text>
     </View>
-  )
+  );
 }
+
+const LotsOfGreetings = () => {
+  return (
+    <View style={[styles.center, {top: 50}]}>
+      <Greeting name='Rexxar' />
+      <Greeting name='Jaina' />
+      <Greeting name='Valeera' />
+    </View>
+  );
+}
+
+export default LotsOfGreetings;
